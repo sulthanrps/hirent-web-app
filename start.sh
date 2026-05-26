1dafb5a455afd5144b9633d223a15b8a8f440739
sulthanrps <sulthanrafips@gmail.com>
2026-05-26 20:31:06 +0700

    fix: grant storage volume write permission to apache

--- start.sh
+++ start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# 1. Hapus link storage lama jika ada, lalu buat ulang agar rapi & tidak error
+chown -R www-data:www-data /var/www/html/storage
+chmod -R 775 /var/www/html/storage
+
 rm -rf public/storage
 php artisan storage:link
 
-# 2. Jalankan migrasi database otomatis
 php artisan migrate --force
 
-# 3. Paksa matikan mesin Apache yang bentrok sesaat sebelum server nyala
 a2dismod mpm_event mpm_worker || true
 a2enmod mpm_prefork || true
 
-# 4. Nyalakan server web
 apache2-foreground
